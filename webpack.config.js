970416eb271e34e11d48c59d3dfd4d3b113ff0cf
José Miguel Sánchez Alés <sio2sio2+github@gmail.com>
2019-08-10 22:10:16 +0200

    Cambio en la configuración de webpack para hacerla más legible

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,70 @@
-// webpack.config.js
+const webpack = require("webpack"),
+      path = require("path"),
+      merge = require("webpack-merge"),
+      HtmlWebPackPlugin = require("html-webpack-plugin"),
+      MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+      name = require("./package.json").name;
+
+
+// Configuración para Babel
+function confBabel(env) {
+   return {
+      module: {
+         rules: [
+            {
+               test: /\.js$/,
+               exclude: /node_modules/,
+               use: {
+                  loader: "babel-loader",
+                  options: {
+                     presets: [["@babel/env", {
+                        debug: env.debug,
+                        corejs: 3,
+                        useBuiltIns: "usage"
+                     }]]
+                  }
+               }
+            },
+         ]
+      }
+   }
+}
+
+
+// Configuración para desarrollo
+// (los mapeos de código fuente en fichero aparte)
+function confDev(filename) {
+   return {
+      devtool: false,
+      devServer: {
+         contentBase: false,
+         open: "chromium",
+      },
+      module: {
+         rules: [
+            {
+               test: /\.(css|sass)$/,
+               use: [MiniCssExtractPlugin.loader,
+                     "css-loader?sourceMap=true",
+                     { 
+                        loader: "postcss-loader",
+                        options: {
+                           plugins: () => [require("autoprefixer")]
+                        }
+                     },
+                     "sass-loader"]
+            }
+         ]
+      },
+      plugins: [
+         new webpack.SourceMapDevToolPlugin({
+            filename: `${filename}.map`
+         })
+      ]
+   }
+}
+
+
 module.exports = env => {
    let mode;
 
@@ -10,5 +76,66 @@ module.exports = env => {
          mode = "production";
    }
 
-   return require(`./webpack/webpack.${mode}.js`)(env);
+   const filename = "js/[name].bundle.js";
+   const common = {
+      mode: mode,
+      entry: {
+         [name]: ["./src/js/index.js"]
+      },
+      output: {
+         path: path.resolve(__dirname, "docs"),
+         filename: filename
+      },
+      resolve: {
+         alias: {
+            app: __dirname
+         }
+      },
+      module: {
+         rules: [
+            {
+               test: /\.html$/,
+               use: {
+                  loader: "html-loader",
+                  options: { minimize: true }
+               }
+            },
+            {
+               test: /\.(css|sass)$/,
+               use: [MiniCssExtractPlugin.loader,
+                     "css-loader",
+                     { 
+                        loader: "postcss-loader",
+                        options: {
+                           plugins: () => [require("autoprefixer")]
+                        }
+                     },
+                     "sass-loader"]
+            },
+            {
+               test: /\.(png|jpe?g|gif|svg)$/i,
+               use: [
+                  'url-loader?limit=4096&name=images/[name].[ext]'
+               ]
+            }
+         ]
+      },
+      plugins: [
+         new webpack.ProvidePlugin({
+            L: "leaflet"
+         }),
+         new HtmlWebPackPlugin({
+            template: "src/index.html",
+         }),
+         new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+            chunkFilename: "[id].css"
+         })
+      ]
+   }
+
+   return merge.smart(
+      common,
+      mode === "production"?confBabel(env):confDev(filename),
+   )
 }
